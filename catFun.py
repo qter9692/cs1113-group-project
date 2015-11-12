36997a5a197f96611802ffd33013bf4a367728d8
Andrew Duran <add5xu@virginia.edu>
2015-11-12 16:31:16 -0500

    part a part b

--- catFun.py
+++ catFun.py
@@ -46,7 +46,7 @@ myimage = dw.loadImage("cat.bmp")
 #
 def updateDisplay(state):
     dw.fill(dw.black)
-    dw.draw(myimage, (state[0], height/2))
+    dw.draw(myimage, (state[0], state[2]))
 
 
 ################################################################
@@ -58,7 +58,7 @@ def updateDisplay(state):
 #
 # state -> state
 def updateState(state):
-    return((state[0]+state[1],state[1]))
+    return(state[0]+state[1],state[1],state[2]+state[3],state[3])
 
 ################################################################
 
@@ -66,10 +66,10 @@ def updateState(state):
 # that is, when pos is less then zero or greater than the screen width
 # state -> bool
 def endState(state):
-    if (state[0] > width or state[0] < 0):
+    if (state[0] > width or state[0] < 0 or state[2] > height or state [2] < 0): 
         return True
     else:
-        return False
+        return False 
 
 
 ################################################################
@@ -88,11 +88,11 @@ def endState(state):
 def handleEvent(state, event):  
 #    print("Handling event: " + str(event))
     if (event.type == pg.MOUSEBUTTONDOWN):
-        if (state[1]) == 1:
-            newState = -1
+        if (state[1],state[3]) == (1,1):
+            newState = (-1,-1)
         else:
-            newState = 1   
-        return((state[0],newState))
+            newState = (1,1)   
+        return((state[0],newState[0],state[2],newState[1]))
     else:
         return(state)
 
@@ -101,7 +101,7 @@ def handleEvent(state, event):
 # World state will be single x coordinate at left edge of world
 
 # The cat starts at the left, moving right 
-initState = (0,1)
+initState = (0,1,0,1)
 
 # Run the simulation no faster than 60 frames per second
 frameRate = 60
